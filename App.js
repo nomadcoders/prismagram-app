20652497057d3234a0b024ea05b6ee5427b9581a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-20 16:46:30 +0900

    #10.4 isLoggedIn part One

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { AppLoading, Font, Asset } from "expo";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
 import { Text, View, AsyncStorage } from "react-native";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { persistCache } from "apollo-cache-persist";
 import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
 import { ApolloProvider } from "react-apollo-hooks";
 import apolloClientOptions from "./apollo";
+import styles from "./styles";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
@@ -26,6 +31,12 @@ export default function App() {
         cache,
         ...apolloClientOptions
       });
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if (isLoggedIn === null || isLoggedIn === false) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
       setLoaded(true);
       setClient(client);
     } catch (e) {
@@ -35,11 +46,13 @@ export default function App() {
   useEffect(() => {
     preLoad();
   }, []);
-  return loaded && client ? (
+  return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ThemeProvider theme={styles}>
+        <View>
+          {isLoggedIn === true ? <Text>I'm in</Text> : <Text>I'm out</Text>}
+        </View>
+      </ThemeProvider>
     </ApolloProvider>
   ) : (
     <AppLoading />
---
+++ styles.js
@@ -0,0 +1,9 @@
+export default {
+  blackColor: "#262626",
+  greyColor: "#F9F9F9",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBlueColor: "#003569"
+};
