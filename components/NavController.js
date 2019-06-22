352e3a44ce597925279e8da9c3d8a55f68037658
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-22 15:34:08 +0900

    #11.6 Navigation Conclusions

--- components/NavController.js
+++ components/NavController.js
@@ -5,7 +5,7 @@ import AuthNavigation from "../navigation/AuthNavigation";
 import MainNavigation from "../navigation/MainNavigation";
 
 export default () => {
-  const isLoggedIn = true;
+  const isLoggedIn = useIsLoggedIn();
   return (
     <View style={{ flex: "1" }}>
       {isLoggedIn ? <MainNavigation /> : <AuthNavigation />}
