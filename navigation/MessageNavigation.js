7d379d0c97746cc9bb2cd81b661757628ccda444
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-22 14:36:44 +0900

    #11.4 Messages Navigation part One

---
+++ navigation/MessageNavigation.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from "react-navigation";
+import Messages from "../screens/Messages/Messages";
+import Message from "../screens/Messages/Message";
+
+export default createStackNavigator({
+  Messages,
+  Message
+});
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,52 @@
-import { View } from "react-native";
-import { createBottomTabNavigator } from "react-navigation";
-import Home from "../screens/Home";
-import Search from "../screens/Search";
-import Notifications from "../screens/Notifications";
-import Profile from "../screens/Profile";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+import Home from "../screens/Tabs/Home";
+import Search from "../screens/Tabs/Search";
+import Notifications from "../screens/Tabs/Notifications";
+import Profile from "../screens/Tabs/Profile";
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InitialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
 
 export default createBottomTabNavigator({
-  Home,
-  Search,
+  Home: {
+    screen: stackFactory(Home, {
+      title: "Home",
+      headerRight: (
+        <TouchableOpacity>
+          <Text>Hello</Text>
+        </TouchableOpacity>
+      )
+    })
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: "Search"
+    })
+  },
   Add: {
     screen: View,
     navigationOptions: {
       tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
     }
   },
-  Notifications,
-  Profile
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications"
+    })
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile"
+    })
+  }
 });
