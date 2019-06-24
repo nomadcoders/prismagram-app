b0b984e1fda5017687efe63d32f359d1bd6ac649
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-24 20:33:51 +0900

    #14.0 Apollo Context

--- App.js
+++ App.js
@@ -32,6 +32,12 @@ export default function App() {
       });
       const client = new ApolloClient({
         cache,
+        request: async operation => {
+          const token = await AsyncStorage.getItem("jwt");
+          return operation.setContext({
+            headers: { Authorization: `Bearer ${token}` }
+          });
+        },
         ...apolloClientOptions
       });
       const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,6 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
 import Loader from "../../components/Loader";
+import { useQuery } from "react-apollo-hooks";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
 
 const View = styled.View`
   justify-content: center;
@@ -8,8 +40,8 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  console.log(loading, data);
+  return <View>{loading ? <Loader /> : null}</View>;
+};
