450c63a644008faa7d1ca7ac5ed645afd35aed7d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-24 21:22:44 +0900

    #15.1 Post Component Swiper

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, Platform } from "react-native";
 import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
+import Swiper from "react-native-swiper";
+import constants from "../constants";
 
 const Container = styled.View``;
 const Header = styled.View`
@@ -19,8 +22,15 @@ const Bold = styled.Text`
 const Location = styled.Text`
   font-size: 12px;
 `;
+const IconsContainer = styled.View`
+  padding: 10px;
+  flex-direction: row;
+`;
+const IconContainer = styled.View`
+  margin-right: 10px;
+`;
 
-const Post = ({ user, location }) => {
+const Post = ({ user, location, files = [] }) => {
   return (
     <Container>
       <Header>
@@ -37,6 +47,40 @@ const Post = ({ user, location }) => {
           </HeaderUserContainer>
         </Touchable>
       </Header>
+      <Swiper
+        showsPagination={false}
+        style={{ height: constants.height / 2.5 }}
+      >
+        {files.map(file => (
+          <Image
+            style={{ width: constants.width, height: constants.height / 2.5 }}
+            key={file.id}
+            source={{ uri: file.url }}
+          />
+        ))}
+      </Swiper>
+      <IconsContainer>
+        <Touchable>
+          <IconContainer>
+            <Ionicons
+              size={28}
+              name={
+                Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"
+              }
+            />
+          </IconContainer>
+        </Touchable>
+        <Touchable>
+          <IconContainer>
+            <Ionicons
+              size={28}
+              name={
+                Platform.OS === "ios" ? "ios-message-empty" : "md-message-empty"
+              }
+            />
+          </IconContainer>
+        </Touchable>
+      </IconsContainer>
     </Container>
   );
 };
