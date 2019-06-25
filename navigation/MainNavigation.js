9b157f638d04bd65c1916629c79ce39e3e679c8e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-25 16:53:00 +0900

    #18.0 Tab Styles

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -6,8 +6,8 @@ import { stackStyles } from "./config";
 
 const MainNavigation = createStackNavigator(
   {
-    TabNavigation,
     PhotoNavigation,
+    TabNavigation,
     MessageNavigation
   },
   {
--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -6,20 +6,50 @@ import SelectPhoto from "../screens/Photo/SelectPhoto";
 import TakePhoto from "../screens/Photo/TakePhoto";
 import UploadPhoto from "../screens/Photo/UploadPhoto";
 import { stackStyles } from "./config";
+import styles from "../styles";
 
 const PhotoTabs = createMaterialTopTabNavigator(
   {
-    SelectPhoto,
-    TakePhoto
+    Select: {
+      screen: SelectPhoto,
+      navigationOptions: {
+        tabBarLabel: "Select"
+      }
+    },
+    Take: {
+      screen: TakePhoto,
+      navigationOptions: {
+        tabBarLabel: "Take"
+      }
+    }
   },
   {
-    tabBarPosition: "bottom"
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      indicatorStyle: {
+        backgroundColor: styles.blackColor,
+        marginBottom: 20
+      },
+      labelStyle: {
+        color: styles.blackColor,
+        fontWeight: "600"
+      },
+      style: {
+        paddingBottom: 20,
+        ...stackStyles
+      }
+    }
   }
 );
 
 export default createStackNavigator(
   {
-    PhotoTabs,
+    Tabs: {
+      screen: PhotoTabs,
+      navigationOptions: {
+        header: null
+      }
+    },
     UploadPhoto
   },
   {
