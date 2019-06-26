dad58b63189b93bb771b3c5ebdeb2fa23669ea5e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-26 16:11:46 +0900

    #18.2 Select Photo part Two

--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -47,7 +47,7 @@ export default createStackNavigator(
     Tabs: {
       screen: PhotoTabs,
       navigationOptions: {
-        header: null
+        title: "Choose Photo"
       }
     },
     UploadPhoto
--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import * as Permissions from "expo-permissions";
-import { Image } from "react-native";
+import { Image, ScrollView } from "react-native";
 import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components";
 import Loader from "../../components/Loader";
+import constants from "../../constants";
 
 const View = styled.View`
-  justify-content: center;
-  align-items: center;
   flex: 1;
 `;
 
@@ -52,10 +51,24 @@ export default () => {
       ) : (
         <View>
           {hasPermission ? (
-            <Image
-              style={{ width: 100, height: 100 }}
-              source={{ uri: selected.uri }}
-            />
+            <>
+              <Image
+                style={{ width: constants.width, height: constants.height / 2 }}
+                source={{ uri: selected.uri }}
+              />
+              <ScrollView contentContainerStyle={{ flexDirection: "row" }}>
+                {allPhotos.map(photo => (
+                  <Image
+                    key={photo.id}
+                    source={{ uri: photo.uri }}
+                    style={{
+                      width: constants.width / 3,
+                      height: constants.height / 6
+                    }}
+                  />
+                ))}
+              </ScrollView>
+            </>
           ) : null}
         </View>
       )}
