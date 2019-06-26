31c2e61c8cd74f416de99ebb309e781e2f5cf46f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-26 16:53:37 +0900

    #18.4 Take Photo part Two

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Camera } from "expo";
+import { Camera } from "expo-camera";
+import { Ionicons } from "@expo/vector-icons";
+import * as Permissions from "expo-permissions";
 import constants from "../../constants";
 import Loader from "../../components/Loader";
+import { TouchableOpacity, Platform } from "react-native";
+import styles from "../../styles";
 
 const View = styled.View`
   flex: 1;
 `;
 
+const Icon = styled.View``;
+
 export default ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
   const askPermission = async () => {
     try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -25,6 +32,13 @@ export default ({ navigation }) => {
       setLoading(false);
     }
   };
+  const toggleType = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
   useEffect(() => {
     askPermission();
   }, []);
@@ -34,8 +48,28 @@ export default ({ navigation }) => {
         <Loader />
       ) : hasPermission ? (
         <Camera
-          style={{ width: constants.width, height: constants.height / 2 }}
-        />
+          type={cameraType}
+          style={{
+            justifyContent: "flex-end",
+            padding: 15,
+            width: constants.width,
+            height: constants.height / 2
+          }}
+        >
+          <TouchableOpacity onPress={toggleType}>
+            <Icon>
+              <Ionicons
+                name={
+                  Platform.OS === "ios"
+                    ? "ios-reverse-camera"
+                    : "md-reverse-camera"
+                }
+                size={32}
+                color={"white"}
+              />
+            </Icon>
+          </TouchableOpacity>
+        </Camera>
       ) : null}
     </View>
   );
