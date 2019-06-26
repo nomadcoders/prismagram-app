fde889b206971cc08338430878f6a23de9a90270
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-26 15:42:30 +0900

    #18.1 Select Photo part One

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as Permissions from "expo-permissions";
+import { Image } from "react-native";
+import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components";
+import Loader from "../../components/Loader";
 
 const View = styled.View`
   justify-content: center;
@@ -9,8 +13,52 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <Text>Select</Text>
-  </View>
-);
+export default () => {
+  const [loading, setLoading] = useState(true);
+  const [hasPermission, setHasPermission] = useState(false);
+  const [selected, setSelected] = useState();
+  const [allPhotos, setAllPhotos] = useState();
+  const getPhotos = async () => {
+    try {
+      const { assets } = await MediaLibrary.getAssetsAsync();
+      const [firstPhoto] = assets;
+      setSelected(firstPhoto);
+      setAllPhotos(assets);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const askPermission = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status === "granted") {
+        setHasPermission(true);
+        getPhotos();
+      }
+    } catch (e) {
+      console.log(e);
+      hasPermission(false);
+    }
+  };
+  useEffect(() => {
+    askPermission();
+  }, []);
+  return (
+    <View>
+      {loading ? (
+        <Loader />
+      ) : (
+        <View>
+          {hasPermission ? (
+            <Image
+              style={{ width: 100, height: 100 }}
+              source={{ uri: selected.uri }}
+            />
+          ) : null}
+        </View>
+      )}
+    </View>
+  );
+};
