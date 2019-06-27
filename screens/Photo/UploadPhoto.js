9e4b32fb0fd4162558ae47df92e4d5fb8e0a42c6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-27 18:00:21 +0900

    #18.7 Preparing for Upload

--- screens/Photo/UploadPhoto.js
+++ screens/Photo/UploadPhoto.js
@@ -1,16 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { Image, ActivityIndicator, Alert } from "react-native";
 import styled from "styled-components";
+import useInput from "../../hooks/useInput";
+import styles from "../../styles";
+import constants from "../../constants";
+import AuthButton from "../../components/AuthButton";
 
 const View = styled.View`
-  justify-content: center;
-  align-items: center;
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const Container = styled.View`
+  padding: 20px;
+  flex-direction: row;
+`;
 
-export default ({ navigation }) => (
-  <View>
-    <Text>I should upload {navigation.getParam("photo").uri}</Text>
-  </View>
-);
+const Form = styled.View`
+  justify-content: flex-start;
+`;
+
+const STextInput = styled.TextInput`
+  margin-bottom: 10px;
+  border: 0px solid ${styles.lightGreyColor};
+  border-bottom-width: 1px;
+  padding-bottom: 10px;
+  width: ${constants.width - 180};
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: ${props => props.theme.blueColor};
+  padding: 10px;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-weight: 600;
+`;
+
+export default ({ navigation }) => {
+  const [loading, setIsLoading] = useState(false);
+  const [fileUrl, setFileUrl] = useState("");
+  const captionInput = useInput("");
+  const locationInput = useInput("");
+  const handleSubmit = async () => {
+    if (captionInput.value === "" || locationInput.value === "") {
+      Alert.alert("All fields are required");
+    }
+  };
+  return (
+    <View>
+      <Container>
+        <Image
+          source={{ uri: navigation.getParam("photo").uri }}
+          style={{ height: 80, width: 80, marginRight: 30 }}
+        />
+        <Form>
+          <STextInput
+            onChangeText={captionInput.onChange}
+            value={captionInput.value}
+            placeholder="Caption"
+            multiline={true}
+            placeholderTextColor={styles.darkGreyColor}
+          />
+          <STextInput
+            onChangeText={locationInput.onChange}
+            value={locationInput.value}
+            placeholder="Location"
+            multiline={true}
+            placeholderTextColor={styles.darkGreyColor}
+          />
+          <Button onPress={handleSubmit}>
+            {loading ? (
+              <ActivityIndicator color="white" />
+            ) : (
+              <Text>Upload </Text>
+            )}
+          </Button>
+        </Form>
+      </Container>
+    </View>
+  );
+};
